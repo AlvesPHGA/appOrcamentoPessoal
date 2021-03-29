7138afcf5a9bcab7d3e7f71378d22ac6ee9a6142
Pedro Henrique Gomes Alves <pedrohenriquegomesalves@gmail.com>
2021-03-29 11:23:23 -0300

    chore: added a for and case block

--- app/records.js
+++ app/records.js
@@ -1,3 +1,42 @@
 function loadRecord() {
-    dataBase.retrieveAllRecords();
+    let despesas= []
+
+    despesas= dataBase.retrieveAllRecords();
+
+    console.log(despesas)
+
+    let expenseList= document.querySelector("#expenseList")
+    
+    despesas.forEach(function (d) {
+        console.log(d)
+
+        let rowList= expenseList.insertRow();
+
+        rowList.insertCell(0).innerHTML= `${d.dia}/${d.mes}/${d.ano}`;
+
+        switch (d.tipo) {
+            case "1":
+                rowList.insertCell(1).innerHTML= 'Alimentação';        
+                break;
+
+            case "2":
+                rowList.insertCell(1).innerHTML= 'Educação';        
+                break;
+
+            case "3":
+                rowList.insertCell(1).innerHTML= 'Lazer';        
+                break;
+
+            case "4":
+                rowList.insertCell(1).innerHTML= 'Saúde';        
+                break;
+
+            case "5":
+                rowList.insertCell(1).innerHTML= 'Transporte';        
+                break;
+        }
+        
+        rowList.insertCell(2).innerHTML= d.descricao;
+        rowList.insertCell(3).innerHTML= d.valor;
+    })
 }
